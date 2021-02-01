3e1cd8c173141ce591456dca2348b7634097ca63
realtalishaw <misstalishawhite@gmail.com>
2021-02-01 16:49:52 +0000

    spaces

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,15 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tmp;
 
+	if (!array)
+	{
+		return;
+	}
+	if (size == 2)
+	{
+		print_array(array, size);
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - 1; j++)
