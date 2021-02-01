7539f129a343ee7ebd1415832a9e29109987b809
realtalishaw <misstalishawhite@gmail.com>
2021-02-01 15:21:48 +0000

    the print statement omg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,18 +12,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, tmp;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
 		for (j =0; j < size - 1; j++)
 		{
-			if (array[j] > array[i])
+			if (array[j] > array[j + 1])
 			{
 				tmp = array[j];
-				array[j] = array[i];
-				array[i] = tmp;
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
 			}
-		print_array(array, size);
-       		}
+
+		}
+
 
 	}
 
